fe1281dabedf794da7eaa413e4119e0eb9aa65b6
David Boedigheimer <stealth259@gmail.com>
2019-11-21 10:38:40 -0800

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 // Back end
+// Business Logic for AddressBook ------------
 function AddressBook() {
   this.contacts = [];
 }
 
+// This new method (addContact), takes an (Contact) object as an argument
+AddressBook.prototype.addContact = function(contact) {
+  // It locates the (AddressBook) contacts array by calling (this.contacts)
+  this.contacts.push(contact);
+  // Uses (.push()) to add the Contact provided as an argument to the (AddressBook) contacts array porperty
+}
+
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
