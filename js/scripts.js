f9194c63526e1e2cc63a475dd7e1112716410bca
David Boedigheimer <stealth259@gmail.com>
2020-01-09 13:16:38 -0800

    Add labels to the addresses.

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,8 @@ Contact.prototype.fullName = function() {
 }
 
 // Business Logic for Addresses
-function HomeAddress(street, city, state) {
+function Address(label, street, city, state) {
+  this.label = label;
   this.street = street;
   this.city = city;
   this.state = state;
@@ -118,6 +119,7 @@ $(document).ready(function() {
   // home address
   $("#home-address").click(function() {
     $("#add-info").append('<div class="home-address">' + '<h2>Home</h2>' +
+                                '<input type="text" value="home" id="home-label" hidden>' +
                                 '<div class="form-group">' +
                                   '<label for="home-street">Street</label>' +
                                   '<input type="text" class="form-control" id="home-street">' +
@@ -135,6 +137,7 @@ $(document).ready(function() {
   // work address
   $("#work-address").click(function() {
     $("#add-info").append('<div class="work-address">' + '<h2>Work</h2>' +
+                                '<input type="text" value="work" id="work-label" hidden>' +
                                 '<div class="form-group">' +
                                   '<label for="work-street">Street</label>' +
                                   '<input type="text" class="form-control" id="work-street">' +
@@ -152,6 +155,7 @@ $(document).ready(function() {
   // other address
   $("#other-address").click(function() {
     $("#add-info").append('<div class="other-address">' + '<h2>Other</h2>' +
+                                '<input type="text" value="other" id="other-label" hidden>' +
                                 '<div class="form-group">' +
                                   '<label for="other-street">Street</label>' +
                                   '<input type="text" class="form-control" id="other-street">' +
@@ -173,6 +177,10 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedPersonalEmail = $("input#new-personal-email").val();
+    // label inputs for addresses
+    var inputtedHomeLabel = $("input#home-label").val();
+    var inputtedWorkLabel = $("input#work-label").val();
+    var inputtedOtherLabel = $("input#other-label").val();
     // home address inputs
     var inputtedHomeStreet = $("input#home-street").val();
     var inputtedHomeCity = $("input#home-city").val();
@@ -190,6 +198,10 @@ $(document).ready(function() {
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-personal-email").val("");
+    // labels for addresses
+    $("input#home-label").val("");
+    $("input#work-label").val("");
+    $("input#other-label").val("");
     // home address inputs
     $("input#home-street").val("");
     $("input#home-city").val("");
@@ -205,7 +217,7 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedPersonalEmail);
     // home address creation
-    var homeAddress = new HomeAddress(inputtedHomeStreet, inputtedHomeCity, inputtedHomeState);
+    var homeAddress = new Address(inputtedHomeStreet, inputtedHomeCity, inputtedHomeState);
     if (inputtedHomeStreet != "" && inputtedHomeCity != "" && inputtedHomeState != "") {
       newContact.homeAddress.push(homeAddress);
     }
